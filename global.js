41c3bec18eed01904b8e1f377cae5ff36f5166bd
Edwin Bartlett <teddybartletg88@gmail.com>
2020-12-01 23:21:46 +0800

    optimized navbar and added settings

--- global.js
+++ global.js
@@ -2,13 +2,18 @@ var navOpen = false;
 
 function openNav() {
     if (navOpen == false) {
-        document.getElementById('heading').innerHTML = "illumi💫";
+        document.getElementById('heading').style.display = "none";
+        document.getElementById('defaultHeading').style.display = "inline";
         document.getElementById('burger').style.display = "none";
         document.getElementById('close').style.display = "inline";
         document.getElementById('sideNav').style.width = "200px";
+        if (screen.width >= 900) {
+            document.getElementById('sideNav').style.width = "270px";
+        }
         navOpen = true;
     } else {
-        document.getElementById('heading').innerHTML = "Discover✨";
+        document.getElementById('heading').style.display = "inline";
+        document.getElementById('defaultHeading').style.display = "none";
         document.getElementById('burger').style.display = "inline";
         document.getElementById('close').style.display = "none";
         document.getElementById('sideNav').style.width = "0";
